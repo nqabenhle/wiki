af26d828640e788b0646e0ecf17c331eccd5b1df
= <=>
2022-07-14 16:00:08 +0200

    Update index, user can click on entry names

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:entry>", views.display_entry, name="display_entry")
